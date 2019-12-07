e3b0d6837366a306a564cf0b180aa8f326e7df55
Johnson Zhong <zhsh@umich.edu>
2019-12-07 02:03:38 -0500

    Implement DLQR

--- meta_contact/util.py
+++ meta_contact/util.py
@@ -1,5 +1,8 @@
 import math
 
+import numpy as np
+import scipy
+
 
 def rotate_wrt_origin(xy, theta):
     return (xy[0] * math.cos(theta) + xy[1] * math.sin(theta),
@@ -22,3 +25,24 @@ def angular_diff_batch(a, b):
     d[d > math.pi] -= 2 * math.pi
     d[d < -math.pi] += 2 * math.pi
     return d
+
+
+def dlqr(A, B, Q, R):
+    """Solve the discrete time lqr controller.
+
+    x[k+1] = A x[k] + B u[k]
+
+    cost = sum x[k].T*Q*x[k] + u[k].T*R*u[k]
+    """
+
+    # ref Bertsekas, p.151
+
+    # first, try to solve the ricatti equation
+    X = np.matrix(scipy.linalg.solve_discrete_are(A, B, Q, R))
+
+    # compute the LQR gain
+    K = np.matrix(scipy.linalg.inv(B.T * X * B + R) * (B.T * X * A))
+
+    eigVals, eigVecs = scipy.linalg.eig(A - B * K)
+
+    return K, X, eigVals
