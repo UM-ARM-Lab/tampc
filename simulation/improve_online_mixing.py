190a7fb9d8edc981c171368fdb91cfd3d2e62dfc
Johnson Zhong <zhsh@umich.edu>
2020-02-24 16:30:07 -0500

    Try kernel smoothing (GP) to mix local and global model

--- simulation/improve_online_mixing.py
+++ simulation/improve_online_mixing.py
@@ -1,17 +1,13 @@
-import numpy as np
+import gpytorch
 import torch
 import logging
-import math
-import os
-import scipy.io
-from gym import wrappers, logger as gym_log
-from arm_pytorch_utilities import rand, load_data, math_utils
+from gym import logger as gym_log
+from arm_pytorch_utilities import rand, load_data
 import matplotlib.pyplot as plt
 
 from meta_contact import online_model
 from meta_contact import prior
 from meta_contact import model
-from meta_contact import cfg
 from arm_pytorch_utilities.model import make
 from arm_pytorch_utilities.make_data import datasource
 
@@ -45,6 +41,24 @@ class PregeneratedDataset(datasource.DataSource):
 
         self._train = xu, y, None
         self._val = self._train
+
+
+class GPWithNominalModelMean(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood, nominal_model):
+        super().__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ZeroMean()
+
+        self.covar_module = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel(
+                lengthscale_constraint=gpytorch.constraints.Interval(0.1, 0.7)
+            ),
+        )
+        self.nominal_model = nominal_model
+
+    def forward(self, x):
+        mean_x = self.nominal_model(x)[:, 0]
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
 
 relearn_dynamics = False
@@ -90,7 +104,7 @@ yy = -0.2 * xx + c + e
 config = load_data.DataConfig(predict_difference=False, predict_all_dims=True, y_in_x_space=False)
 ds_local = PregeneratedDataset(xx.view(-1, 1), uu, yy.view(-1, 1), config=config)
 
-# fit local model (if we hand assigned mixing weights)
+# linear local model (if we hand assigned mixing weights)
 dynamics = online_model.OnlineDynamicsModel(0.1, pm, ds_local, torch.sub, local_mix_weight_scale=1000,
                                             const_local_mix_weight=True)
 yhat_linear_fit = dynamics.predict(None, None, x.view(-1, 1), u)
@@ -105,6 +119,43 @@ dynamics.const_local_weight = False
 dynamics.local_weight_scale = 50.
 dynamics.characteristic_length = 0.8
 yhat_new1 = dynamics.predict(None, None, x.view(-1, 1), u)
+
+# GP local model
+# initialize likelihood and model
+likelihood = gpytorch.likelihoods.GaussianLikelihood()
+train_x = xx
+train_y = yy
+model = GPWithNominalModelMean(train_x, train_y, likelihood, pm.dyn_net.predict)
+training_iter = 50
+
+# Find optimal model hyperparameters
+model.train()
+likelihood.train()
+optimizer = torch.optim.Adam([
+    {'params': model.parameters()},  # Includes GaussianLikelihood parameters
+], lr=0.1)
+# "Loss" for GPs - the marginal log likelihood
+mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+for i in range(training_iter):
+    # Zero gradients from previous iteration
+    optimizer.zero_grad()
+    # Output from model
+    output = model(train_x)
+    # Calc loss and backprop gradients
+    loss = -mll(output, train_y)
+    loss.backward()
+    print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
+        i + 1, training_iter, loss.item(),
+        model.covar_module.base_kernel.lengthscale.item(),
+        model.likelihood.noise.item()
+    ))
+    optimizer.step()
+# Get into evaluation (predictive posterior) mode
+model.eval()
+likelihood.eval()
+with torch.no_grad(), gpytorch.settings.fast_pred_var():
+    f_pred = model(x)
+    observed_pred = likelihood(f_pred)
 
 # plt.scatter(x.numpy(), y.numpy(), alpha=0.2)
 # plt.plot(x.numpy(), yhat.numpy(), alpha=0.2)
@@ -122,7 +173,17 @@ plt.scatter(xx.numpy(), yy.numpy(), alpha=0.2)
 plt.plot(x.numpy(), yhat_linear_fit.numpy(), label='linear fit', alpha=0.2)
 # plt.plot(x.numpy(), yhat_baseline_mix.numpy(), label='baseline fit high w')
 # plt.plot(x.numpy(), yhat_baseline_mix2.numpy(), label='baseline fit low w')
-plt.plot(x.numpy(), yhat_new1.numpy(), label='our fit')
+# plt.plot(x.numpy(), yhat_new1.numpy(), label='our fit')
+
+with torch.no_grad():
+    # Get upper and lower confidence bounds
+    lower, upper = observed_pred.confidence_region()
+    # Plot training data as black stars
+    # Plot predictive means as blue line
+    plt.plot(x.numpy(), observed_pred.mean.numpy(), label='GP mean')
+    # Shade between the lower and upper confidence bounds
+    plt.fill_between(x.numpy(), lower.numpy(), upper.numpy(), alpha=0.2, label='GP 2 std')
+
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('local data and models')
