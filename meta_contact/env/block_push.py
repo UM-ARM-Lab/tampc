98073d4ad3cca39b98ac6ebc510856dae0cec78f
Johnson Zhong <zhsh@umich.edu>
2020-03-30 20:32:24 -0400

    Parameterize state cost for done

--- meta_contact/env/block_push.py
+++ meta_contact/env/block_push.py
@@ -402,7 +402,7 @@ class PushAgainstWallEnv(MyPybulletEnv):
 
     def __init__(self, goal=(1.0, 0.), init_pusher=(-0.25, 0), init_block=(0., 0.), init_yaw=0.,
                  environment_level=0, sim_step_wait=None, mini_steps=100, wait_sim_steps_per_mini_step=20,
-                 max_pusher_force=20, debug_visualizations=None,
+                 max_pusher_force=20, debug_visualizations=None, state_cost_for_done=0.06,
                  reaction_force_strategy=ReactionForceStrategy.MEDIAN_OVER_MINI_STEPS, **kwargs):
         """
         :param goal:
@@ -427,6 +427,7 @@ class PushAgainstWallEnv(MyPybulletEnv):
         self.mini_steps = mini_steps
         self.wait_sim_step_per_mini_step = wait_sim_steps_per_mini_step
         self.reaction_force_strategy = reaction_force_strategy
+        self.state_cost_for_done = state_cost_for_done
 
         # initial config
         self.goal = None
@@ -710,7 +711,7 @@ class PushAgainstWallEnv(MyPybulletEnv):
         diff = self.state_difference(state, self.goal)
         diff = diff.reshape(-1)
         cost = diff @ self.Q @ diff
-        done = cost < 0.04
+        done = cost < self.state_cost_for_done
         if action is not None:
             cost += action @ self.R @ action
         return cost, done
