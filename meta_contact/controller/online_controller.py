710a4a838523977cdd8357d7068717fc7766f886
Johnson Zhong <zhsh@umich.edu>
2020-07-13 18:03:18 -0400

    include current x in x history to get same performance

--- meta_contact/controller/online_controller.py
+++ meta_contact/controller/online_controller.py
@@ -80,7 +80,7 @@ class OnlineMPC(controller.MPC):
         t = len(self.u_history)
         x = obs
         if t > 0:
-            self.dynamics.update(self.x_history[-1], self.u_history[-1], x)
+            self.dynamics.update(self.x_history[-2], self.u_history[-1], x)
 
         self.mpc_cost_history.append(self.mpc.running_cost(x.view(1, -1), None))
         u = self._compute_action(x)
@@ -197,15 +197,16 @@ class OnlineMPPI(OnlineMPC, controller.MPPI_MPC):
 
             # cooldown on entering and leaving traps (can't use our progress during recovery for calculating whether we
             # enter a trap or not since the recovery policy isn't expected to decrease goal cost)
-            cur_index = len(self.x_history)
+            cur_index = len(self.x_history) - 1
             if cur_index - self.autonomous_recovery_end_index < (self.nonnominal_dynamics_trend_len - 1):
                 return False
 
             # check cost history compared to before we entered non-nominal dyanmics and after we've entered
             # don't include the first state since the reaction forces are not initialized correctly
             before_trend = torch.cat(self.orig_cost_history[
-                                     max(1, self.nonnominal_dynamics_start_index - self.nonnominal_dynamics_trend_len):
-                                     self.nonnominal_dynamics_start_index])
+                                     max(1,
+                                         self.nonnominal_dynamics_start_index + 1 - self.nonnominal_dynamics_trend_len):
+                                     self.nonnominal_dynamics_start_index + 1])
             current_trend = torch.cat(self.orig_cost_history[-self.nonnominal_dynamics_trend_len:])
             # should be negative
             before_progress_rate = (before_trend[1:] - before_trend[:-1]).mean()
@@ -293,21 +294,21 @@ class OnlineMPPI(OnlineMPC, controller.MPPI_MPC):
 
         self.using_local_model_for_nonnominal_dynamics = True
         # includes the current observation
-        self.nonnominal_dynamics_start_index = len(self.x_history) + 1
+        self.nonnominal_dynamics_start_index = len(self.x_history) - 1
 
         self.dynamics.use_temp_local_nominal_model()
         # update the local model with the last transition for entering the mode
-        self.dynamics.update(self.x_history[-1], self.u_history[-1], x)
+        self.dynamics.update(self.x_history[-2], self.u_history[-1], x)
 
     def _start_recovery_mode(self):
         logger.debug("Entering autonomous recovery mode")
         self.autonomous_recovery_mode = True
         # TODO also make autonomous recovery index include this observation
-        self.autonomous_recovery_start_index = len(self.x_history) + 1
+        self.autonomous_recovery_start_index = len(self.x_history)
 
         # avoid these points in the future
         for i in range(min(len(self.u_history), self.steps_before_entering_trap_to_avoid)):
-            self.trap_set.append((self.x_history[-i - 1], self.u_history[-i - 1]))
+            self.trap_set.append((self.x_history[-i - 2], self.u_history[-i - 1]))
         temp = torch.stack([torch.cat((x, u)) for x, u in self.trap_set])
         logger.debug("trap set updated to be\n%s", temp)
 
@@ -316,7 +317,8 @@ class OnlineMPPI(OnlineMPC, controller.MPPI_MPC):
             # change mpc cost
             # TODO parameterize how far back in history to return to
             nominal_dynamics_set = torch.stack(
-                self.x_history[max(0, self.nonnominal_dynamics_start_index - 10):self.nonnominal_dynamics_start_index])
+                self.x_history[
+                max(0, self.nonnominal_dynamics_start_index + 1 - 10):self.nonnominal_dynamics_start_index + 1])
             nominal_return_cost = cost.CostQRSet(nominal_dynamics_set, self.Q_recovery, self.R, self.compare_to_goal)
 
             if self.autonomous_recovery is AutonomousRecovery.RETURN_STATE:
@@ -343,7 +345,7 @@ class OnlineMPPI(OnlineMPC, controller.MPPI_MPC):
         logger.debug("Leaving autonomous recovery mode")
         logger.debug(torch.tensor(self.dynamics_class_history[-self.leave_recovery_num_turns:]))
         self.autonomous_recovery_mode = False
-        self.autonomous_recovery_end_index = len(self.x_history) + 1
+        self.autonomous_recovery_end_index = len(self.x_history)
 
         # deprecated
         # if we're sure that we've left an unrecognized class, save as recovery
