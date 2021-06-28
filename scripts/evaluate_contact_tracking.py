f9aa4a0d38353ae08d0f0f4e49b55b1d68009bb5
Johnson Zhong <zhsh@umich.edu>
2021-06-28 15:39:20 -0400

    Save figures and close env to fix memory leak issue

--- scripts/evaluate_contact_tracking.py
+++ scripts/evaluate_contact_tracking.py
@@ -29,7 +29,7 @@ from tampc.env_getters.arm import ArmGetter
 ch = logging.StreamHandler()
 fh = logging.FileHandler(os.path.join(cfg.ROOT_DIR, "logs", "{}.log".format(datetime.now())))
 
-logging.basicConfig(level=logging.WARNING,
+logging.basicConfig(level=logging.INFO,
                     format='[%(levelname)s %(asctime)s %(pathname)s:%(lineno)d] %(message)s',
                     datefmt='%m-%d %H:%M:%S', handlers=[ch, fh])
 
@@ -42,14 +42,18 @@ NO_CONTACT_ID = -1
 
 prefix_to_environment = {'arm/gripper': arm.FloatingGripperEnv}
 
+all_res = {}
 
-def extract_env_and_level_from_string(string) -> typing.Optional[typing.Tuple[typing.Type[env_base.PybulletEnv], int]]:
+
+def extract_env_and_level_from_string(string) -> typing.Optional[
+    typing.Tuple[typing.Type[env_base.PybulletEnv], int, int]]:
     for name, env_cls in prefix_to_environment.items():
         m = re.search(r"{}\d+".format(name), string)
         if m is not None:
             # find level, which is number succeeding this string match
             level = int(m.group()[len(name):])
-            return env_cls, level
+            seed = int(os.path.splitext(os.path.basename(string))[0])
+            return env_cls, level, seed
 
     return None
 
@@ -61,7 +65,7 @@ def load_file(datafile, show_in_place=False):
     ret = extract_env_and_level_from_string(datafile)
     if ret is None:
         raise RuntimeError(f"Path not properly formatted to extract environment and level")
-    env_cls, level = ret
+    env_cls, level, seed = ret
 
     # load data
     d = scipy.io.loadmat(datafile)
@@ -83,7 +87,7 @@ def load_file(datafile, show_in_place=False):
     freespace_ratio = unique_contact_counts[NO_CONTACT_ID] / steps_taken
     if len(unique_contact_counts) < 2 or freespace_ratio > 0.95:
         raise RuntimeWarning(f"Too few contacts; spends {freespace_ratio} ratio in freespace")
-    print(f"{datafile} freespace ratio {freespace_ratio} unique contact IDs {unique_contact_counts}")
+    logger.info(f"{datafile} freespace ratio {freespace_ratio} unique contact IDs {unique_contact_counts}")
 
     obj_poses = {}
     for unique_obj_id in unique_contact_counts.keys():
@@ -153,6 +157,7 @@ def load_file(datafile, show_in_place=False):
     for i in range(len(X) - 1):
         c = contact_set.update(x[i], u[i], env.state_difference(x[i + 1], x[i]).reshape(-1), r[i + 1])
         labels[i] = -1 if c is None else hash(c)
+    env.close()
 
     # TODO cluster iteratively, using our tracking to move the points to get fairer baselines
     # simple baselines
@@ -164,26 +169,37 @@ def load_file(datafile, show_in_place=False):
 
     # we care about homogenity more than completeness - multiple objects in a single cluster is more dangerous
     h, c, v = clustering_metrics(contact_id[:-1], kmeans.labels_, beta=0.5)
-    print(f"kmeans h {h} c {c} v {v}")
+    logger.info(f"kmeans h {h} c {c} v {v}")
     h, c, v = clustering_metrics(contact_id[:-1], dbscan.labels_, beta=0.5)
-    print(f"dbscan h {h} c {c} v {v}")
+    logger.info(f"dbscan h {h} c {c} v {v}")
     h, c, v = clustering_metrics(contact_id[:-1], labels, beta=0.5)
-    print(f"ours h {h} c {c} v {v}")
+    logger.info(f"ours h {h} c {c} v {v}")
+    all_res[(level, seed)] = h, c, v
 
     # simplified plot in 2D
     def cluster_id_to_str(cluster_id):
         return 'no contact' if cluster_id == NO_CONTACT_ID else 'contact {}'.format(cluster_id)
 
-    # ground truth
-    plot_cluster_res(contact_id, X, f"Task {level} {datafile.split('/')[-1]} ground truth",
-                     label_function=cluster_id_to_str)
+    save_loc = "/home/zhsh/Documents/results/2021-06-22/6 clustering res/"
+
+    def save_and_close_fig(f, name):
+        plt.savefig(os.path.join(save_loc, f"{level} {seed} {name}"))
+        plt.close(f)
+
+    # # ground truth
+    f = plot_cluster_res(contact_id, X, f"Task {level} {datafile.split('/')[-1]} ground truth",
+                         label_function=cluster_id_to_str)
+    save_and_close_fig(f, 'gt')
 
     # comparison of methods
-    plot_cluster_res(kmeans.labels_, xx, f"Task {level} {datafile.split('/')[-1]} K-means")
-    plot_cluster_res(dbscan.labels_, xx, f"Task {level} {datafile.split('/')[-1]} DBSCAN")
-    plot_cluster_res(labels, xx, f"Task {level} {datafile.split('/')[-1]} ours")
-
-    plt.show()
+    f = plot_cluster_res(kmeans.labels_, xx, f"Task {level} {datafile.split('/')[-1]} K-means")
+    save_and_close_fig(f, 'kmeans')
+    f = plot_cluster_res(dbscan.labels_, xx, f"Task {level} {datafile.split('/')[-1]} DBSCAN")
+    save_and_close_fig(f, 'dbscan')
+    f = plot_cluster_res(labels, xx, f"Task {level} {datafile.split('/')[-1]} ours")
+    save_and_close_fig(f, 'ours')
+    #
+    # plt.show()
 
     return X, U, dX, contact_id, obj_poses, reactions
 
@@ -211,6 +227,7 @@ def plot_cluster_res(labels, xx, name, label_function=None):
             ax.scatter(pos[:, 0], pos[:, 1])
     set_position_axis_bounds(ax, 0.7)
     ax.legend()
+    return f
 
 
 def set_position_axis_bounds(ax, bound=0.7):
@@ -221,20 +238,29 @@ def set_position_axis_bounds(ax, bound=0.7):
 
 
 data = {}
-full_dir = os.path.join(cfg.DATA_DIR, 'arm/gripper10')
 
-files = os.listdir(full_dir)
-files = sorted(files)
+dirs = ['arm/gripper10', 'arm/gripper11', 'arm/gripper12', 'arm/gripper13']
 
-for file in files:
-    full_filename = '{}/{}'.format(full_dir, file)
-    # some interesting ones filtered
-    if file not in ['16.mat', '18.mat', '22.mat']:
-        continue
-    if os.path.isdir(full_filename):
-        continue
-    try:
-        data[full_filename] = load_file(full_filename)
-    except (RuntimeError, RuntimeWarning) as e:
-        print(f"{full_filename} error: {e}")
-        continue
+for res_dir in dirs:
+    # full_dir = os.path.join(cfg.DATA_DIR, 'arm/gripper10')
+    full_dir = os.path.join(cfg.DATA_DIR, res_dir)
+
+    files = os.listdir(full_dir)
+    files = sorted(files)
+
+    for file in files:
+        full_filename = '{}/{}'.format(full_dir, file)
+        # # some interesting ones filtered
+        # if file not in ['16.mat', '18.mat', '22.mat']:
+        #     continue
+        if os.path.isdir(full_filename):
+            continue
+        try:
+            data[full_filename] = load_file(full_filename)
+        except (RuntimeError, RuntimeWarning) as e:
+            logger.info(f"{full_filename} error: {e}")
+            continue
+
+for k, v in all_res.items():
+    pretty_v = [round(metric, 2) for metric in v]
+    logger.info(f"{k} : {pretty_v}")
