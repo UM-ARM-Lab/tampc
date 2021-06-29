5b6e0fa1783156a9f0d6df4c9b78c80be32424e1
Johnson Zhong <zhsh@umich.edu>
2021-06-29 12:02:44 -0400

    Implement an online version of kmeans

--- cottun/cluster_baseline.py
+++ cottun/cluster_baseline.py
@@ -1,4 +1,8 @@
 from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
+# from tampc.contact import ContactSet
+import copy
+import numpy as np
 
 
 class KMeansWithAutoK:
@@ -18,3 +22,48 @@ class KMeansWithAutoK:
             if k > 1 and sse[-1] > self.inertia_ratio * sse[-2]:
                 return KMeans(n_clusters=k - 1, **self.kmeans_args).fit(x)
         return KMeans(n_clusters=self.max_k, **self.kmeans_args).fit(x)
+
+
+class OnlineSklearnContactSet:
+    """Variant of contact set where we replace our clustering method with an sklearn based one
+
+    One concern is that the clustering won't be stable between updates since the label ID is meaningless"""
+
+    def __init__(self, cluster_method, inertia_ratio=0.8, delay_new_clusters_until=5):
+        self.last_labels = None
+        self.cluster_method = cluster_method
+        # self.cluster_method = MiniBatchKMeans(n_clusters=1)
+        self.inertia_ratio = inertia_ratio
+        self.delay_new_clusters_until = delay_new_clusters_until
+
+        self.data = None
+
+    def update(self, x, u, dx, reaction):
+        xx = np.concatenate([x[:2], reaction, u]).reshape(1, -1)
+        if self.data is None:
+            self.data = xx
+            self.cluster_method.fit(self.data)
+        else:
+            self.data = np.concatenate([self.data, xx], axis=0)
+
+            # decide if data should be part of existing clusters or if a new cluster should be created
+            use_same_n_clusters = self.cluster_method
+            use_more_n_clusters = copy.deepcopy(self.cluster_method)
+            use_more_n_clusters.n_clusters += 1
+
+            use_same_n_clusters.fit(self.data)
+            use_more_n_clusters.fit(self.data)
+            # using more cluster improved inertia sufficiently, so use new one
+            if len(self.data) >= self.delay_new_clusters_until and \
+                    use_more_n_clusters.inertia_ < use_same_n_clusters.inertia_ * self.inertia_ratio:
+                self.cluster_method = use_more_n_clusters
+            else:
+                self.cluster_method = use_same_n_clusters
+
+        # filter by moving all members of the current cluster by dx
+        this_cluster = self.cluster_method.labels_[-1]
+        members_of_this_cluster = self.cluster_method.labels_ == this_cluster
+        # don't move everything if reaction force too low
+        if np.linalg.norm(reaction) > 2:
+            self.data[members_of_this_cluster, :2] += dx[:2]
+        return self.cluster_method.labels_
--- scripts/evaluate_contact_tracking.py
+++ scripts/evaluate_contact_tracking.py
@@ -12,6 +12,7 @@ import typing
 
 import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
+from sklearn.cluster import KMeans
 from sklearn import metrics
 
 from arm_pytorch_utilities.optim import get_device
@@ -21,7 +22,7 @@ from tampc.env import pybullet_env as env_base
 from tampc.env import arm
 from tampc.env_getters.arm import ArmGetter
 
-from cottun.cluster_baseline import KMeansWithAutoK
+from cottun.cluster_baseline import KMeansWithAutoK, OnlineSklearnContactSet
 
 ch = logging.StreamHandler()
 fh = logging.FileHandler(os.path.join(cfg.ROOT_DIR, "logs", "{}.log".format(datetime.now())))
@@ -82,6 +83,18 @@ def sklearn_method_factory(method, **kwargs):
         # give it some help by telling it the number of clusters as unique contacts IDs
         res = method(**kwargs).fit(xx)
         return res.labels_
+
+    return sklearn_method
+
+
+def online_sklearn_method_factory(method, inertia_ratio=0.5, **kwargs):
+    def sklearn_method(X, U, reactions, env_class):
+        online_method = OnlineSklearnContactSet(method(**kwargs), inertia_ratio=inertia_ratio)
+        labels = None
+        for i in range(len(X) - 1):
+            labels = online_method.update(X[i], U[i], env_class.state_difference(X[i + 1], X[i]).reshape(-1),
+                                          reactions[i + 1])
+        return labels
 
     return sklearn_method
 
@@ -227,9 +240,10 @@ if __name__ == "__main__":
 
     dirs = ['arm/gripper10', 'arm/gripper11', 'arm/gripper12', 'arm/gripper13']
     methods_to_run = {
-        'ours': our_method,
-        'kmeans': sklearn_method_factory(KMeansWithAutoK),
-        'dbscan': sklearn_method_factory(DBSCAN, eps=0.5, min_samples=10)
+        # 'ours': our_method,
+        # 'kmeans': sklearn_method_factory(KMeansWithAutoK),
+        # 'dbscan': sklearn_method_factory(DBSCAN, eps=0.5, min_samples=10),
+        'online-kmeans': online_sklearn_method_factory(KMeans, n_clusters=1, random_state=0)
     }
 
     for res_dir in dirs:
