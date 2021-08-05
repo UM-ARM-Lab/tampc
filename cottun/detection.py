df76566226c845c627c34956b73f4d4d5566f4be
Johnson Zhong <zhsh@umich.edu>
2021-08-05 19:07:00 -0400

    Retrieve using last pose if not specified

--- cottun/detection.py
+++ cottun/detection.py
@@ -48,13 +48,15 @@ class ContactDetector:
         """Return contact point in link frame that most likely explains the observed residual"""
         # TODO if single pass evaluation doesn't work (e.g. from points being too sparse), try iteratively resampling
 
-    def get_last_contact_location(self, pose, **kwargs):
+    def get_last_contact_location(self, pose=None, **kwargs):
         """Get last contact point given the current end effector pose"""
         if len(self.observation_history) == 0:
             return None
         in_contact, ee_force_torque, prev_pose = self.observation_history[-1]
         if not in_contact:
             return None
+        if pose is None:
+            pose = prev_pose
 
         # in link frame
         last_contact_point = self.isolate_contact(ee_force_torque, prev_pose, **kwargs)
