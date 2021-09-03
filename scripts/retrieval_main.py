b15ae1c0eeaa1c4de44fb4bde837456add19f920
Johnson Zhong <zhsh@umich.edu>
2021-09-03 18:03:11 -0400

    Refactor script into function calls to facilitate using different seeds

--- scripts/retrieval_main.py
+++ scripts/retrieval_main.py
@@ -91,6 +91,7 @@ def sample_model_points(object_id, num_points=100, reject_too_close=0.002, force
 
 
 def test_icp(env):
+    z = env._observe_ee(return_z=True)[-1]
     # test ICP using fixed set of points
     o = p.getBasePositionAndOrientation(env.target_object_id)[0]
     contact_points = np.stack([
@@ -146,19 +147,6 @@ def test_icp(env):
         time.sleep(0.2)
 
 
-force_gui = True
-env = RetrievalGetter.env(level=Levels.SIMPLE_CLUTTER, mode=p.GUI if force_gui else p.DIRECT)
-contact_params = RetrievalGetter.contact_parameters(env)
-
-
-def cost_to_go(state, goal):
-    return env.state_distance_two_arg(state, goal)
-
-
-def create_contact_object():
-    return tracking.ContactUKF(None, contact_params)
-
-
 def object_robot_penetration_score(object_id, robot_id, object_transform):
     """Compute the penetration between object and robot for a given transform of the object"""
     o_pos, o_orientation = p.getBasePositionAndOrientation(object_id)
@@ -175,101 +163,145 @@ def object_robot_penetration_score(object_id, robot_id, object_transform):
     return -d
 
 
-using_soft_contact = True
-using_predetermined_control = True
+from arm_pytorch_utilities.math_utils import angular_diff
 
-dtype = torch.float32
-if using_soft_contact:
-    contact_set = tracking.ContactSetSoft(arm.ArmPointToConfig(env), contact_params)
-else:
-    contact_set = tracking.ContactSetHard(contact_params, contact_object_factory=create_contact_object)
 
-# TODO have this seed be selectable
-rand.seed(0)
-if using_predetermined_control:
-    predetermined_control = {}
+def pose_error(target_pose, guess_pose):
+    # mirrored, so being off by 180 degrees is fine
+    yaw_error = min(abs(angular_diff(target_pose[-1], guess_pose[-1])),
+                    abs(angular_diff(target_pose[-1] + np.pi, guess_pose[-1])))
+    pos_error = np.linalg.norm(np.subtract(target_pose[:2], guess_pose[:2]))
+    return pos_error, yaw_error
 
-    ctrl = [[0.7, -1]] * 5
-    ctrl += [[0.4, 0.4], [.5, -1]] * 6
-    ctrl += [[-0.2, 1]] * 4
-    ctrl += [[0.3, -0.3], [0.4, 1]] * 4
-    ctrl += [[1., -1]] * 3
-    ctrl += [[1., 0.6], [-0.7, 0.5]] * 4
-    ctrl += [[0., 1]] * 5
-    ctrl += [[1., 0]] * 4
-    ctrl += [[0.4, -1.], [0.4, 0.5]] * 4
-    noise = (np.random.rand(len(ctrl), 2) - 0.5) * 0.5
-    ctrl = np.add(ctrl, noise)
-    predetermined_control[Levels.SIMPLE_CLUTTER] = ctrl
 
-    ctrl = RetrievalPredeterminedController(env.contact_detector, contact_set,
-                                            predetermined_control[env.level])
-else:
-    ds, pm = RetrievalGetter.prior(env, use_tsf=UseTsf.NO_TRANSFORM)
-    u_min, u_max = env.get_control_bounds()
-    ctrl = RetrievalController(env.contact_detector, env.nu, pm.dyn_net, cost_to_go, contact_set, u_min, u_max,
-                               walk_length=6)
+def run_retrieval(env, seed=0, using_soft_contact=True, using_predetermined_control=True):
+    contact_params = RetrievalGetter.contact_parameters(env)
 
-obs = env.reset()
-z = env._observe_ee(return_z=True)[-1]
+    def cost_to_go(state, goal):
+        return env.state_distance_two_arg(state, goal)
 
-model_points = sample_model_points(env.target_object_id, num_points=50, force_z=z, seed=0, name="cheezit")
-mp = model_points[:, :2].cpu().numpy()
-mph = model_points.clone().to(dtype=dtype)
-mph[:, -1] = 1
+    def create_contact_object():
+        return tracking.ContactUKF(None, contact_params)
 
-ctrl.set_goal(env.goal[:2])
-info = None
-simTime = 0
-best_tsf_guess = None
-while True:
-    best_distance = None
-    simTime += 1
-    env.draw_user_text("{}".format(simTime), xy=(0.5, 0.7, -1))
+    dtype = torch.float32
 
-    action = ctrl.command(obs, info)
-    env.visualize_contact_set(contact_set)
-    if env.contact_detector.in_contact():
-        pts = contact_set.get_posterior_points() if using_soft_contact else None
-        to_iter = contact_set.get_hard_assignment(
-            contact_set.p.hard_assignment_threshold) if using_soft_contact else contact_set
-        dist_per_est_obj = []
-        for c in to_iter:
-            this_pts = pts[c] if using_soft_contact else c.points
-            T, distances, _ = icp.icp_3(this_pts, model_points[:, :2], given_init_pose=best_tsf_guess, batch=30)
-            T = T.inverse()
-            penetration = [object_robot_penetration_score(env.target_object_id, env.robot_id, T[b]) for b in
-                           range(T.shape[0])]
-            score = np.abs(penetration)
-            best_tsf_index = np.argmin(score)
+    if using_soft_contact:
+        contact_set = tracking.ContactSetSoft(arm.ArmPointToConfig(env), contact_params)
+    else:
+        contact_set = tracking.ContactSetHard(contact_params, contact_object_factory=create_contact_object)
 
-            yaws = rot_2d_mat_to_angle(T)
-            translations = T[:, :2, 2]
-            best_tsf_distances = (translations.var(dim=0).sum()).item()
-            # best_tsf_distances = distances[best_tsf_index].mean().item()
+    rand.seed(0)
+    if using_predetermined_control:
+        predetermined_control = {}
 
-            dist_per_est_obj.append(best_tsf_distances)
-            if best_distance is None or best_tsf_distances < best_distance:
-                best_distance = best_tsf_distances
-                best_tsf_guess = T[best_tsf_index].inverse()
+        ctrl = [[0.7, -1]] * 5
+        ctrl += [[0.4, 0.4], [.5, -1]] * 6
+        ctrl += [[-0.2, 1]] * 4
+        ctrl += [[0.3, -0.3], [0.4, 1]] * 4
+        ctrl += [[1., -1]] * 3
+        ctrl += [[1., 0.6], [-0.7, 0.5]] * 4
+        ctrl += [[0., 1]] * 5
+        ctrl += [[1., 0]] * 4
+        ctrl += [[0.4, -1.], [0.4, 0.5]] * 4
+        noise = (np.random.rand(len(ctrl), 2) - 0.5) * 0.5
+        ctrl = np.add(ctrl, noise)
+        predetermined_control[Levels.SIMPLE_CLUTTER] = ctrl
 
-            # for b in range(T.shape[0]):
-            #     transformed_model_points = mph @ T[b].transpose(-1, -2)
-            #     for i, pt in enumerate(transformed_model_points):
-            #         if i % 2 == 0:
-            #             pt = [pt[0], pt[1], z]
-            #             env._dd.draw_point(f"tmpt{b}-{i}", pt, color=(0, 1, b / T.shape[0]), length=0.003)
+        rand.seed(seed)
+        for k, v in predetermined_control.items():
+            predetermined_control[k] = np.add(v, (np.random.rand(len(v), 2) - 0.5) * 0.02)
 
-        logger.info(f"err each obj {np.round(dist_per_est_obj, 4)}")
-        transformed_model_points = mph @ best_tsf_guess.inverse().transpose(-1, -2)
-        for i, pt in enumerate(transformed_model_points):
-            if i % 2 == 0:
-                continue
-            pt = [pt[0], pt[1], z]
-            env._dd.draw_point(f"tmptbest-{i}", pt, color=(0, 0, 1), length=0.008)
+        ctrl = RetrievalPredeterminedController(env.contact_detector, contact_set,
+                                                predetermined_control[env.level])
+    else:
+        ds, pm = RetrievalGetter.prior(env, use_tsf=UseTsf.NO_TRANSFORM)
+        u_min, u_max = env.get_control_bounds()
+        rand.seed(seed)
+        ctrl = RetrievalController(env.contact_detector, env.nu, pm.dyn_net, cost_to_go, contact_set, u_min, u_max,
+                                   walk_length=6)
 
-    if torch.is_tensor(action):
-        action = action.cpu()
+    obs = env.reset()
+    z = env._observe_ee(return_z=True)[-1]
 
-    action = np.array(action).flatten()
-    obs, rew, done, info = env.step(action)
+    model_points = sample_model_points(env.target_object_id, num_points=50, force_z=z, seed=0, name="cheezit")
+    mp = model_points[:, :2].cpu().numpy()
+    mph = model_points.clone().to(dtype=dtype)
+    mph[:, -1] = 1
+
+    ctrl.set_goal(env.goal[:2])
+    info = None
+    simTime = 0
+    best_tsf_guess = None
+    pose_error_per_step = {}
+
+    while True:
+        best_distance = None
+        simTime += 1
+        env.draw_user_text("{}".format(simTime), xy=(0.5, 0.7, -1))
+
+        action = ctrl.command(obs, info)
+        env.visualize_contact_set(contact_set)
+        if env.contact_detector.in_contact():
+            pts = contact_set.get_posterior_points() if using_soft_contact else None
+            to_iter = contact_set.get_hard_assignment(
+                contact_set.p.hard_assignment_threshold) if using_soft_contact else contact_set
+            dist_per_est_obj = []
+            for c in to_iter:
+                this_pts = pts[c] if using_soft_contact else c.points
+                T, distances, _ = icp.icp_3(this_pts, model_points[:, :2], given_init_pose=best_tsf_guess, batch=30)
+                T = T.inverse()
+                penetration = [object_robot_penetration_score(env.target_object_id, env.robot_id, T[b]) for b in
+                               range(T.shape[0])]
+                score = np.abs(penetration)
+                best_tsf_index = np.argmin(score)
+
+                # pick object with lowest variance in its translation estimate
+                translations = T[:, :2, 2]
+                best_tsf_distances = (translations.var(dim=0).sum()).item()
+
+                dist_per_est_obj.append(best_tsf_distances)
+                if best_distance is None or best_tsf_distances < best_distance:
+                    best_distance = best_tsf_distances
+                    best_tsf_guess = T[best_tsf_index].inverse()
+
+                # for b in range(T.shape[0]):
+                #     transformed_model_points = mph @ T[b].transpose(-1, -2)
+                #     for i, pt in enumerate(transformed_model_points):
+                #         if i % 2 == 0:
+                #             pt = [pt[0], pt[1], z]
+                #             env._dd.draw_point(f"tmpt{b}-{i}", pt, color=(0, 1, b / T.shape[0]), length=0.003)
+
+            logger.info(f"err each obj {np.round(dist_per_est_obj, 4)}")
+            best_T = best_tsf_guess.inverse()
+
+            target_pose = p.getBasePositionAndOrientation(env.target_object_id)
+            yaw = p.getEulerFromQuaternion(target_pose[1])[-1]
+            target_pose = [target_pose[0][0], target_pose[0][1], yaw]
+
+            guess_pose = [best_T[0, 2].item(), best_T[1, 2].item(), rot_2d_mat_to_angle(best_T.view(1, 3, 3)).item()]
+            pos_err, yaw_err = pose_error(target_pose, guess_pose)
+
+            pose_error_per_step[simTime] = pos_err + 0.3 * yaw_err
+            logger.info(f"pose error {simTime}: {pos_err} {yaw_err} {pose_error_per_step[simTime]}")
+            transformed_model_points = mph @ best_T.transpose(-1, -2)
+            for i, pt in enumerate(transformed_model_points):
+                if i % 2 == 0:
+                    continue
+                pt = [pt[0], pt[1], z]
+                env._dd.draw_point(f"tmptbest-{i}", pt, color=(0, 0, 1), length=0.008)
+
+        if torch.is_tensor(action):
+            action = action.cpu()
+
+        action = np.array(action).flatten()
+        obs, rew, done, info = env.step(action)
+
+
+def main():
+    force_gui = True
+    env = RetrievalGetter.env(level=Levels.SIMPLE_CLUTTER, mode=p.GUI if force_gui else p.DIRECT)
+    run_retrieval(env, seed=1)
+
+
+if __name__ == "__main__":
+    main()
